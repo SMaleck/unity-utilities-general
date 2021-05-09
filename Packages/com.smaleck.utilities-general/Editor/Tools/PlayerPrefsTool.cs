5a45e6d7982f101c9dcce0df99a1657ff5a59fb5
Sebastian Maleck <s.maleck@live.de>
2021-05-09 22:16:50 +0200

    Cleanup

--- Packages/com.smaleck.utilities-general/Editor/Tools/PlayerPrefsTool.cs
+++ Packages/com.smaleck.utilities-general/Editor/Tools/PlayerPrefsTool.cs
@@ -4,9 +4,9 @@ using UtilitiesGeneral.Editor.Constants;
 
 namespace UtilitiesGeneral.Editor.Tools
 {
-    public class PlayerPrefsTool : MonoBehaviour
+    public class PlayerPrefsTool
     {
-        [MenuItem(MenuConstants.MenuRoot + "PlayerPrefs - Clear")]
+        [MenuItem(MenuConstants.MenuRoot + "/PlayerPrefs - Clear")]
         public static void ClearPlayerPrefs()
         {
             PlayerPrefs.DeleteAll();
